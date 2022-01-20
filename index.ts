c9ff57cb2e01d25ad69da1b2d6309f28c6128716
Walter <wrkagel@uw.edu>
2022-01-20 06:07:34 -0800

    changed port to 8080 to match docker

--- index.ts
+++ index.ts
@@ -2,9 +2,9 @@ import express from 'express';
 import EmployeeDao, { EmployeeDaoImpl } from './dao/employee-dao';
 import cors, { CorsOptions } from 'cors';
 import ReimbursementDao, { ReimbursementDaoImpl } from './dao/reimbursement-dao';
-import https from 'https'
+//import https from 'https'
 import http from 'http';
-import fs from 'fs'
+//import fs from 'fs'
 import ReimbursementService from './services/reimbursement-service-interface';
 import ReimbursementServiceImpl from './services/reimbursement-services';
 import expressErrorHandler from './middleware/express-error-handler';
@@ -38,10 +38,10 @@ app.use(errLogger);
 app.use(expressErrorHandler);
 
 const httpServer = http.createServer(app);
-const httpsServer = https.createServer({
-    key:fs.readFileSync("privkey.pem"),
-    cert:fs.readFileSync("cert.pem")
-},app);
+// const httpsServer = https.createServer({
+//     key:fs.readFileSync("privkey.pem"),
+//     cert:fs.readFileSync("cert.pem")
+// },app);
 
-httpServer.listen(80, ()=>console.log("HTTPServer listening on port 80"));
-httpsServer.listen(443, () => console.log("HTTPSServer listening on port 443"));
+httpServer.listen(8080, ()=>console.log("HTTPServer listening on port 80"));
+// httpsServer.listen(443, () => console.log("HTTPSServer listening on port 443"));
