0d58ea1585cca2873d1b5ea288ade91842204b35
Walter <wrkagel@uw.edu>
2022-01-20 07:42:12 -0800

    Transitioning to Azure Web App.

--- index.ts
+++ index.ts
@@ -2,9 +2,6 @@ import express from 'express';
 import EmployeeDao, { EmployeeDaoImpl } from './dao/employee-dao';
 import cors, { CorsOptions } from 'cors';
 import ReimbursementDao, { ReimbursementDaoImpl } from './dao/reimbursement-dao';
-//import https from 'https'
-import http from 'http';
-//import fs from 'fs'
 import ReimbursementService from './services/reimbursement-service-interface';
 import ReimbursementServiceImpl from './services/reimbursement-services';
 import expressErrorHandler from './middleware/express-error-handler';
@@ -37,11 +34,4 @@ app.use(createJsonRouter(reimbursementService));
 app.use(errLogger);
 app.use(expressErrorHandler);
 
-const httpServer = http.createServer(app);
-// const httpsServer = https.createServer({
-//     key:fs.readFileSync("privkey.pem"),
-//     cert:fs.readFileSync("cert.pem")
-// },app);
-
-httpServer.listen(80, ()=>console.log("HTTPServer listening on port 80"));
-// httpsServer.listen(443, () => console.log("HTTPSServer listening on port 443"));
+app.listen(process.env.PORT, () => "app listening on port: " + process.env.PORT)
