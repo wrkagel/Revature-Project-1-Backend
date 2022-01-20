3f530b6a0e4a29e2795831203d51125611414b66
Walter <wrkagel@uw.edu>
2022-01-20 08:45:14 -0800

    Attempting to make azure wb app https.

--- index.ts
+++ index.ts
@@ -9,6 +9,8 @@ import reqLogger from './middleware/req-logger';
 import errLogger from './middleware/error-logger';
 import createFileRouter from './routers/file-router';
 import createJsonRouter from './routers/json-router';
+import https from 'https';
+import fs from 'fs';
 
 const app = express();
 
@@ -34,4 +36,9 @@ app.use(createJsonRouter(reimbursementService));
 app.use(errLogger);
 app.use(expressErrorHandler);
 
-app.listen(process.env.PORT, () => "app listening on port: " + process.env.PORT)
+const httpsServer = https.createServer({
+    key: fs.readFileSync("privkey.pem"),
+    cert: fs.readFileSync("cert.pem")
+},app);
+
+httpsServer.listen(process.env.PORT, () => "app listening on port: " + process.env.PORT)
